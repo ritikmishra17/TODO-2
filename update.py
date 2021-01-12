a1bbb6349636edb83df606b6e5a638aa09b78ae6
Ritik Mishra <33049461+ritikmishra17@users.noreply.github.com>
2021-01-12 22:21:33 +0530

    Update update.py

--- update.py
+++ update.py
@@ -1,9 +1,11 @@
 from app import app, db
-from flask import url_for, redirect
+from flask import url_for, redirect, Blueprint
 from model import Todo
 
+update_bp = Blueprint('update_bp', __name__)
 
-@app.route("/update/<int:todo_id>")
+
+@update_bp.route("/update/<int:todo_id>")
 def update(todo_id):
     todo = Todo.query.filter_by(id=todo_id).first()
     todo.complete = not todo.complete
